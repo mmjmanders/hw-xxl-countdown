643c18051d3dcaeb9e661f4f155bc34e2f8c8268
Mark Manders <mmjmanders@gmail.com>
2025-12-18 11:30:09 +0100

    Configure BASE_URL support in Vite and enable manual GitHub Actions workflow dispatch

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const { BASE_URL } = process.env
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: BASE_URL || '/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
