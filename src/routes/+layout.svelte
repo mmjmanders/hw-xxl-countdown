a4674e61f1cc0282cdf6ff2d8d9a13f1895db6e0
Mark Manders <mmjmanders@gmail.com>
2025-12-24 13:31:54 +0100

    Integrate `@vite-pwa/sveltekit` (#21)
    * Integrate `@vite-pwa/sveltekit`
    
    Move web manifest logic to runtime using `virtual:pwa-info`, and remove static manifest file.
    
    * Enable `autoUpdate` for PWA configuration in `vite.config.ts`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,16 @@
 	import '@fontsource-variable/open-sans';
 	import '@fontsource-variable/google-sans-code';
 	import './layout.css';
+	import { pwaInfo } from 'virtual:pwa-info';
 
 	let { children } = $props();
+	const webManifestHref = $derived(pwaInfo?.webManifest?.href || undefined);
 </script>
+
+<svelte:head>
+	{#if webManifestHref}
+		<link rel="manifest" href={webManifestHref} />
+	{/if}
+</svelte:head>
 
 {@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		SvelteKitPWA({
+			devOptions: {
+				enabled: true
+			},
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'HW XX(L) Countdown',
+				short_name: 'HW XX(L) Countdown',
+				icons: [
+					{
+						src: 'web-app-manifest-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: ['any']
+					},
+					{
+						src: 'web-app-manifest-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: ['any']
+					}
+				],
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				display: 'standalone'
+			}
+		})
+	]
 });
