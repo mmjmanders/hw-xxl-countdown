2d67532b254de5d3407fe5d928b4e4b42114ba64
Mark Manders <mmjmanders@gmail.com>
2025-12-24 11:00:31 +0100

    Migrate from Vue 3 to Svelte
    Replace Tailwind CSS integration, and clean up project files.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { PUBLIC_HW_DATE } from '$env/static/public';
+	import dayjs from 'dayjs';
+
+	const departureDate = $state(dayjs(PUBLIC_HW_DATE));
+	let now = $state(dayjs());
+	const diff = $derived.by(() => departureDate.diff(now));
+	const days = $derived.by(() => Math.floor(diff / (24 * 60 * 60 * 1000)));
+	const hours = $derived.by(() => Math.floor((diff / (60 * 60 * 1000)) % 24));
+	const minutes = $derived.by(() => Math.floor((diff / (60 * 1000)) % 60));
+	const seconds = $derived.by(() => Math.floor((diff / 1000) % 60));
+
+	$effect(() => {
+		const interval = setInterval(() => {
+			now = dayjs();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<div class="flex flex-col items-center gap-4 p-2 text-ireland sm:gap-6">
+	<h1 class="text-3xl font-bold sm:text-5xl">HW XX(L) Countdown</h1>
+	<div class="flex w-full max-w-2xl justify-between gap-4 sm:gap-6">
+		<div class="flex flex-col items-center gap-4 sm:gap-6">
+			<span class="font-mono text-6xl sm:text-8xl">{days}</span>
+			<span class="text-xl sm:text-3xl">dagen</span>
+		</div>
+		<div class="flex flex-col items-center gap-4 sm:gap-6">
+			<span class="font-mono text-6xl sm:text-8xl">{hours}</span>
+			<span class="text-xl sm:text-3xl">uren</span>
+		</div>
+		<div class="flex flex-col items-center gap-4 sm:gap-6">
+			<span class="font-mono text-6xl sm:text-8xl">{minutes}</span>
+			<span class="text-xl sm:text-3xl">minuten</span>
+		</div>
+		<div class="flex flex-col items-center gap-4 sm:gap-6">
+			<span class="font-mono text-6xl sm:text-8xl">{seconds}</span>
+			<span class="text-xl sm:text-3xl">seconden</span>
+		</div>
+	</div>
+</div>
